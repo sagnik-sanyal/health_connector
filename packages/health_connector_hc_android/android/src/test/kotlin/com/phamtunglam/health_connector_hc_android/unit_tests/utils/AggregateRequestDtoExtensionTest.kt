1d1682fe44f55cb1f2b31b0b328251936b9ffe61
Pham Tung Lam <fam.tung.lam@gmail.com>
2026-01-02 11:04:14 +0100

    test(health_connector_hc_android): Add Kotlin unit unit tests for `AggregateRequestDtoExtension`

---
+++ packages/health_connector_hc_android/android/src/test/kotlin/com/phamtunglam/health_connector_hc_android/unit_tests/utils/AggregateRequestDtoExtensionTest.kt
@@ -0,0 +1,93 @@
+package com.phamtunglam.health_connector_hc_android.unit_tests.utils
+
+import com.phamtunglam.health_connector_hc_android.pigeon.AggregateRequestDto
+import com.phamtunglam.health_connector_hc_android.pigeon.AggregationMetricDto
+import com.phamtunglam.health_connector_hc_android.pigeon.BloodPressureAggregateRequestDto
+import com.phamtunglam.health_connector_hc_android.pigeon.BloodPressureHealthDataTypeDto
+import com.phamtunglam.health_connector_hc_android.pigeon.CommonAggregateRequestDto
+import com.phamtunglam.health_connector_hc_android.pigeon.HealthDataTypeDto
+import com.phamtunglam.health_connector_hc_android.utils.dataType
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+
+/**
+ * Unit tests for AggregateRequestDto Extensions.
+ *
+ * Tests verify proper extraction of common fields from [AggregateRequestDto] sealed class
+ * and its subclasses ([CommonAggregateRequestDto] and [BloodPressureAggregateRequestDto]).
+ * These extensions work around a Pigeon limitation where sealed classes cannot have fields.
+ */
+@DisplayName("AggregateRequestDtoExtensions")
+class AggregateRequestDtoExtensionTest {
+
+    private companion object {
+        const val TEST_START_TIME = 1609459200000L // 2021-01-01 00:00:00 UTC
+        const val TEST_END_TIME = 1609545600000L // 2021-01-02 00:00:00 UTC
+    }
+
+    // region CommonAggregateRequestDto Tests
+
+    @Nested
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+    @DisplayName("CommonAggregateRequestDto")
+    inner class CommonAggregateRequestDtoTests {
+
+        @Test
+        @DisplayName(
+            "GIVEN CommonAggregateRequestDto with all fields → " +
+                "WHEN all extensions accessed → " +
+                "THEN returns all correct values",
+        )
+        fun whenCommonRequest_thenAllExtensionsWork() {
+            // Given
+            val request = CommonAggregateRequestDto(
+                startTime = TEST_START_TIME,
+                endTime = TEST_END_TIME,
+                aggregationMetric = AggregationMetricDto.AVG,
+                dataType = HealthDataTypeDto.HEART_RATE_SERIES_RECORD,
+            )
+
+            // When & Then
+            request.startTime shouldBe TEST_START_TIME
+            request.endTime shouldBe TEST_END_TIME
+            request.aggregationMetric shouldBe AggregationMetricDto.AVG
+            request.dataType shouldBe HealthDataTypeDto.HEART_RATE_SERIES_RECORD
+        }
+    }
+
+    // endregion
+
+    // region BloodPressureAggregateRequestDto Tests
+
+    @Nested
+    @DisplayName("BloodPressureAggregateRequestDto")
+    inner class BloodPressureAggregateRequestDtoTests {
+
+        @Test
+        @DisplayName(
+            "GIVEN BloodPressureAggregateRequestDto with all fields → " +
+                "WHEN all extensions accessed → " +
+                "THEN returns all correct values",
+        )
+        fun whenBloodPressureRequest_thenAllExtensionsWork() {
+            // Given
+            val request = BloodPressureAggregateRequestDto(
+                aggregationMetric = AggregationMetricDto.AVG,
+                bloodPressureDataType = BloodPressureHealthDataTypeDto.DIASTOLIC,
+                startTime = TEST_START_TIME,
+                endTime = TEST_END_TIME,
+            )
+
+            // When & Then
+            request.startTime shouldBe TEST_START_TIME
+            request.endTime shouldBe TEST_END_TIME
+            request.aggregationMetric shouldBe AggregationMetricDto.AVG
+            request.dataType shouldBe HealthDataTypeDto.BLOOD_PRESSURE
+        }
+    }
+
+    // endregion
+}
